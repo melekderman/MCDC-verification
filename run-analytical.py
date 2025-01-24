21cae355e2527a6d845327e4fa3e4d6a6a49891b
ilhamv <variansyah.ilham@gmail.com>
2025-01-24 12:16:37 -0800

    separate analytical and benchmark tests

--- run-analytical.py
+++ run-analytical.py
@@ -1,14 +1,13 @@
 import os, argparse
 
 # Option parser
-parser = argparse.ArgumentParser(description="MC/DC verification")
+parser = argparse.ArgumentParser(description="MC/DC Verification - Analytical")
 parser.add_argument("--srun", type=int, default=0)
 parser.add_argument("--mpiexec", type=int, default=0)
 parser.add_argument("--mpirun", type=int, default=0)
 args, unargs = parser.parse_known_args()
 
-# Analytical verification
-os.chdir("analytical")
+# Get the MPI option
 if args.srun > 0 or args.mpiexec > 0 or args.mpirun > 0:
     if args.srun > 1:
         mpi_option = "--srun=%i" % args.srun
@@ -16,4 +15,7 @@ if args.srun > 0 or args.mpiexec > 0 or args.mpirun > 0:
         mpi_option = "--mpiexec=%i" % args.mpiexec
     elif args.mpirun > 1:
         mpi_option = "--mpirun=%i" % args.mpirun
+
+# Analytical verification
+os.chdir("analytical")
 os.system("python run.py %s" % mpi_option)
