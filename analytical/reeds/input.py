66e8f871de70697afe6e63847b095b2b7b7807ca
Melek Derman <48313913+melekderman@users.noreply.github.com>
2025-01-25 01:21:33 -0800

    add source and tally

--- analytical/reeds/input.py
+++ analytical/reeds/input.py
@@ -5,28 +5,29 @@ import numpy as np
 # Set model
 # =============================================================================
 
-#MSet materials
-m1 = mcdc.material(capture=np.array([0.1]), scatter=np.array([0.9]))
-m2 = mcdc.material(capture=np.array([0.1]), scatter=np.array([0.9]))
+#Set materials
+m1 = mcdc.material(capture=np.array([50.0]), scatter=np.array([0.0]))
+m2 = mcdc.material(capture=np.array([5.0]), scatter=np.array([0.0]))
 m3 = mcdc.material(capture=np.array([0.0]), scatter=np.array([0.0]))
-m4 = mcdc.material(capture=np.array([5.0]), scatter=np.array([0.0]))
-m5 = mcdc.material(capture=np.array([50.0]), scatter=np.array([0.0]))
+m4 = mcdc.material(capture=np.array([1.0]), scatter=np.array([0.9]))
+m5 = mcdc.material(capture=np.array([1.0]), scatter=np.array([0.9]))
 
 #Set surfaces
-s1 = mcdc.surface(surface_type='plane-x', x=0.0,  bc='vacuum')
+s1 = mcdc.surface(surface_type='plane-x', x=0.0, bc='reflective')
 s2 = mcdc.surface(surface_type='plane-x', x=2.0, bc='interface')
 s3 = mcdc.surface(surface_type='plane-x', x=3.0, bc='interface')
 s4 = mcdc.surface(surface_type='plane-x', x=5.0, bc='interface')
 s5 = mcdc.surface(surface_type='plane-x', x=6.0, bc='interface')
-s6 = mcdc.surface(surface_type='plane-x', x=8.0, bc='reflective')
+s6 = mcdc.surface(surface_type='plane-x', x=8.0, bc='vacuum')
 
 #Set cells
-c1 = mcdc.cell()
-c2 = 
-c3 = 
-c4 = 
-c5 = 
- 
+c1 = mcdc.cell(+s1 & -s2, m1)
+c2 = mcdc.cell(+s2 & -s3, m2)
+c3 = mcdc.cell(+s3 & -s4, m3)
+c4 = mcdc.cell(+s4 & -s5, m4)
+c5 = mcdc.cell(+s5 & -s6, m5)
+
+
 # =============================================================================
 # Set source
 # =============================================================================
