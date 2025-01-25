42d9d4a35ce27c60851297c97a6390a889be8e4e
Melek Derman <48313913+melekderman@users.noreply.github.com>
2025-01-24 18:53:59 -0800

    update for material definition

---
+++ analytical/reeds/input.py
@@ -0,0 +1,28 @@
+import mcdc
+import numpy as np
+
+# =============================================================================
+# Set model
+# =============================================================================
+
+#Materials
+m1 = mcdc.material(capture=np.array([0.1]), scatter=np.array([0.9]))
+m2 = mcdc.material(capture=np.array([0.1]), scatter=np.array([0.9]))
+m3 = mcdc.material(capture=np.array([0.0]), scatter=np.array([0.0]))
+m4 = mcdc.material(capture=np.array([5.0]), scatter=np.array([0.0]))
+m5 = mcdc.material(capture=np.array([50.0]), scatter=np.array([0.0]))
+
+#Surfaces
+
+
+#Cells
+
+
+# =============================================================================
+# Set source
+# =============================================================================
+
+
+# =============================================================================
+# Set tally, setting, and run mcdc
+# =============================================================================
