77ccdb7744777ab3a3dcadd7d2c3cceab516c4fb
Melek Derman <48313913+melekderman@users.noreply.github.com>
2025-03-28 14:49:00 -0700

    add convergence func for k_eig. these may need to be updated after fixing the bank_buff type issue

--- analytical/tool.py
+++ analytical/tool.py
@@ -44,3 +44,24 @@ def plot_convergence(name, N_particle, error, error_max):
     # Create folder if not there yet
     plt.savefig(name + ".png")
     plt.clf()
+
+def plot_convergence_k(name, N_particle, error):
+    mid = int(len(N_particle) / 2)
+
+    plt.plot(N_particle, error, "bo", fillstyle="none", label="2-norm")
+
+    line = 1.0 / np.sqrt(N_particle)
+    line *= error[mid] / line[mid]
+    plt.plot(N_particle, line, "r--", label=r"$O(N^{-0.5})$")
+
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.ylabel("Relative error")
+    plt.xlabel(r"# of histories, $N$")
+    plt.legend()
+    plt.grid()
+    plt.title(name)
+
+    # Create folder if not there yet
+    plt.savefig(name + ".png")
+    plt.clf()
